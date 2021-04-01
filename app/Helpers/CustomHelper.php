15752b84e85f637b6b5ea240941891a523185fd3
Hans Marcon <yourcapslock@gmail.com>
2021-04-01 10:08:53 +0800

    Initial

---
+++ app/Helpers/CustomHelper.php
@@ -0,0 +1,21 @@
+<?php
+
+if(!function_exists('asset2')){
+
+	function asset2($pathToFile){
+		return URL::to('/public/').$pathToFile;
+	}
+
+}
+
+if(!function_exists('link2')){
+
+	function link2($url){
+		return env('APP_URL').$url;
+	}	
+
+}
+
+
+
+?>
